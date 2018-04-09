449ad4acc18bd8c0721ff0b40808191f959a1c5c
Korek302 <jacek_blady@interia.pl>
2018-04-09 04:20:12 +0200

    09.04.18
    todo: znajdzki, przeszkody, ruszajace sie platformy, komunikat po smierci, pozostawianie zycia przy spadku

---
+++ Assets/scripts/hpManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets._2D;
+
+public class hpManager : MonoBehaviour
+{
+    public GameObject[] hearts;
+    public Rigidbody2D characterBody;
+    public Platformer2DUserControl playerController;
+
+    private int hp;
+    
+	void Start ()
+    {
+        hp = hearts.Length;
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            knockBack();
+            hp--;
+            hearts[hp].SetActive(false);
+        }
+        if(hp < 1)
+        {
+            SceneManager.LoadScene("GameLvl1");
+        }
+    }
+
+    private void knockBack()
+    {
+        StartCoroutine("haltMovement");
+        Vector2 vector = new Vector2(Random.Range(-5.0f, -1.0f), Random.Range(0.0f, 3.0f));
+        characterBody.velocity = vector;
+    }
+
+    IEnumerator haltMovement()
+    {
+        playerController.movingEnabled = false;
+        yield return new WaitForSeconds(1.0f);
+        playerController.movingEnabled = true;
+    }
+}
---
+++ Assets/scripts/victoryManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityStandardAssets._2D;
+
+public class victoryManager : MonoBehaviour
+{
+    public int LvlNo;
+    public GameObject PlayAgainButton;
+    public Button PlayAgainButton2;
+    public GameObject LvlFinishPanel;
+    public Platformer2DUserControl PlayerController;
+    public Animator Animator;
+    private const int finalLvl = 2;
+
+    private void Start()
+    {
+        PlayAgainButton.SetActive(false);
+        LvlFinishPanel.SetActive(false);
+        PlayAgainButton2.onClick.AddListener
+        (
+            () => { RestartGame(); }
+        );
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayerController.movingEnabled = false;
+            Animator.speed = 0;
+
+            if(LvlNo == finalLvl)
+            {
+                PlayAgainButton.SetActive(true);
+            }
+            else
+            {
+                LvlFinishPanel.SetActive(true);
+                StartCoroutine("jumpToNewLayer");
+            }
+        }
+    }
+
+    public void RestartGame()
+    {
+        LvlNo = 1;
+        SceneManager.LoadScene("GameLvl" + LvlNo);
+    }
+
+    IEnumerator jumpToNewLayer()
+    {
+        yield return new WaitForSeconds(2.0f);
+        LvlNo++;
+        SceneManager.LoadScene("GameLvl" + LvlNo);
+    }
+}
